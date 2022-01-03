a912c306d16c4e64570c25949a026ed251cc3661
Lee-Dayoung <ekdud8236@gmail.com>
2022-01-03 17:47:35 +0900

    Day03_05

--- src/main/java/com/springbook/biz/board/impl/BoardDAOSpring.java
+++ src/main/java/com/springbook/biz/board/impl/BoardDAOSpring.java
@@ -1,9 +1,12 @@
 package com.springbook.biz.board.impl;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import com.springbook.biz.board.BoardVO;
@@ -43,5 +46,21 @@ public class BoardDAOSpring {
 	public List<BoardVO> getBoardList(BoardVO vo) {
 		System.out.println("===> Spring JDBC로 getBoard() 기능 처리");
 		return jdbcTemplate.query(BOARD_LIST, new BoardRowMapper());
+	}
+	
+	class BoardRowMapper implements RowMapper<BoardVO> {
+
+		@Override
+		public BoardVO mapRow(ResultSet rs, int rowNum) throws SQLException {
+			BoardVO board = new BoardVO();
+			board.setSeq(rs.getInt("SEQ"));
+			board.setTitle(rs.getString("TITLE"));
+			board.setWriter(rs.getString("WRITER"));
+			board.setContent(rs.getString("CONTENT"));
+			board.setRegDate(rs.getDate("REGDATE"));
+			board.setCnt(rs.getInt("CNT"));
+			return board;
+		}
+
 	}
 }	
